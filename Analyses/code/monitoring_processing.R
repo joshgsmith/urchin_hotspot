b967bb2bdc7224ac75421970fac59e345e697f78
Joshua G. Smith <joshsmith@nceas.ucsb.edu>
2022-12-16 09:01:25 -0800

    processing

--- Analyses/code/monitoring_processing.R
+++ Analyses/code/monitoring_processing.R
@@ -6,19 +6,24 @@ rm(list=ls())
 require(dplyr)
 
 #set directories
-datadir <- "/Users/Joshua/Box Sync/Data/RC_EM_2010to2019/raw"
+datadir <- "/Users/Joshua/Box Sync/Data"
 dataout <- "/Users/Joshua/Box Sync/Data/RC_EM_2010to2019/processed"
-figdir 
+figdir <- "/Users/Joshua/Box Sync/hotspot_analyses/Analyses/figures"
+tabdir <- "/Users/Joshua/Box Sync/hotspot_analyses/Analyses/tables"
 
 
 ################################################################################
 #import data
 
-mlpa_swath <- read.csv(file.path(datadir, "MLPA_kelpforest_swath.4.csv"))
-rcca_ab <- read.csv(file.path(datadir, "RCCA_abalone_size_data.csv"))
-rcca_invert_swath <- read.csv(file.path(datadir, "RCCA_invertebrate_swath_data.csv"))
-rcca_urchin_size <- read.csv(file.path(datadir, "RCCA_urchin_size_data.csv"))
+mlpa_swath <- read.csv(file.path(datadir, "RC_EM_2010to2019/raw/MLPA_kelpforest_swath.4.csv"))
+rcca_ab <- read.csv(file.path(datadir, "RC_EM_2010to2019/raw/RCCA_abalone_size_data.csv"))
+rcca_invert_swath <- read.csv(file.path(datadir, "RC_EM_2010to2019/raw/RCCA_invertebrate_swath_data.csv"))
+rcca_urchin_size <- read.csv(file.path(datadir, "RC_EM_2010to2019/raw/RCCA_urchin_size_data.csv"))
+mpa_traits <- read.csv(file.path(datadir, "mpa_traits/mpa_attributes_clean.csv"))
 
+
+################################################################################
+#
 
 
 
