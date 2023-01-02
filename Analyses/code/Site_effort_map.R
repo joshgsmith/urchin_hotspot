dae7e2c4acc8599dc76080f7a98298640e1ff566
Joshua G. Smith <joshsmith@nceas.ucsb.edu>
2023-01-02 14:08:39 -0800

    process site locations

---
+++ Analyses/code/Site_effort_map.R
@@ -0,0 +1,14 @@
+#Joshua G. Smith
+#December 7, 2022
+
+rm(list=ls())
+
+require(dplyr)
+
+#set directories
+datadir <- "/Users/Joshua/Box Sync/Data"
+dataout <- "/Users/Joshua/Box Sync/Data/RC_EM_2010to2019/processed"
+figdir <- "/Users/Joshua/Box Sync/hotspot_analyses/Analyses/figures"
+tabdir <- "/Users/Joshua/Box Sync/hotspot_analyses/Analyses/tables"
+
+
---
+++ Analyses/code/site_locations_processing.R
@@ -0,0 +1,66 @@
+#Joshua G. Smith
+#January 2, 2022
+
+rm(list=ls())
+
+require(dplyr)
+
+datadir <- "/Users/Joshua/Box Sync/Data"
+dataout <- "/Users/Joshua/Box Sync/Data/gis_data/processed"
+figdir <- "/Users/Joshua/Box Sync/hotspot_analyses/Analyses/figures"
+tabdir <- "/Users/Joshua/Box Sync/hotspot_analyses/Analyses/tables"
+
+################################################################################
+#read data
+
+mlpa_swath <- read.csv(file.path(datadir, "monitoring_processed/mlpa_swath.csv"))
+rcca_invert_swath <- read.csv(file.path(datadir, "monitoring_processed/rcca_invert_swath.csv"))
+rcca_kelp_swath <- read.csv(file.path(datadir, "monitoring_processed/rcca_kelp_swath.csv"))
+rcca_urchin_sizefq <- read.csv(file.path(datadir, "monitoring_processed/rcca_urchin_sizefq.csv"))
+
+################################################################################
+#select sites
+
+mlpa_swath1 <- mlpa_swath %>%
+                mutate(program = "mlpa",
+                       method = "invert swath",
+                       restoration_site = "no",
+                       survey_type = "annual")%>%
+                dplyr::distinct(program, method, survey_type,
+                              year, site = affiliated_mpa,
+                             restoration_site, latitude, longitude) 
+
+rcca_invert_swath1 <- rcca_invert_swath %>%
+                mutate(program = "rcca",
+                       method = "invert swath")%>%
+                dplyr::distinct(program, method, survey_type,
+                              year, site = site_new,
+                              restoration_site,
+                latitude, longitude) 
+
+rcca_kelp_swath1 <- rcca_kelp_swath %>%
+  mutate(program = "rcca",
+         method = "kelp swath")%>%
+  dplyr::distinct(program, method, survey_type,
+                  year, site = site_new,
+                  restoration_site,
+                  latitude, longitude) 
+
+rcca_urchin_sizefq1 <- rcca_urchin_sizefq %>%
+  mutate(program = "rcca",
+         method = "size fq")%>%
+  dplyr::distinct(program, method, survey_type,
+                  year, site = site_new,
+                  restoration_site,
+                  latitude, longitude) 
+
+
+site_locations <- rbind(mlpa_swath1, rcca_invert_swath1, rcca_kelp_swath1,
+                       rcca_urchin_sizefq1)
+
+
+saveRDS(site_locations, file.path(dataout, "site_locations.Rds"))
+
+
+
+
